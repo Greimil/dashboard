bcb8a44490a0ab338ece4edafed74b8d35bd070a
Greimil <greimilnunez@gmail.com>
2022-09-11 16:37:22 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#2893AD", 
+        "secondary": "#FAA352",
+        "terciary": "#e2e8f0"
+
+      }
+    },
+  },
+  plugins: [],
+}
